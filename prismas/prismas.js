c8aee3cdc3e8ee97e38b5a9259674b4d90845076
Jean Florimont <flojeanc@yahoo.fr>
2018-03-04 00:13:36 +0100

    woehf

--- prismas/prismas.js
+++ prismas/prismas.js
@@ -1,35 +1,42 @@
 // var create_cube = function(scene, size, x, y, z)
+// create_cylinder = function(scene, height, diameter, tessellation, num_border, x, y, z)
 
-var primes_size_x = 10;
-var primes_size_y = 10;
+var primes_size_x = 20;
+var primes_size_z = 20;
 
 var createScene = function(canvas, engine){
 	// create a basic BJS Scene object
 	var scene = new BABYLON.Scene(engine);
-	var camera = camera1(scene, canvas);
+	var camera = camera2(scene, canvas);
 	var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
 
-	// create a built-in "sphere" shape; its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation 
-
-	// move the sphere upward 1/2 of its height
-
-	// create a built-in "ground" shape;
 
 	var primes = [];
-	var space = 0;
+	var space = -0.28;
 	for (i=0; i< primes_size_x; i++)
 	{
 		primes[i] = [];
 		console.log("create primes[" + i + "]")
-		for (j=0; j<primes_size_y; j++)
+		for (j=0; j<primes_size_z; j++)
 		{
-			console.log("create primes[" + i + "][" + j + "]")
-			primes[i][j] = create_cube(scene, 1, i + i*space, randomFloat(0.0, 1.0), j + j*space);
+			// console.log("create primes[" + i + "][" + j + "]")
+			primes[i][j] = create_cylinder(scene, 1, 1, 4, x, y ,z);
+			primes[i][j].rotate(BABYLON.Axis.Y, Math.PI / 4, BABYLON.Space.LOCAL);
+
+			// generate position
+			var x = i + i*space - primes_size_x/2;
+			var y = 0;
+			var z = j + j*space - primes_size_z/2;
+
+			// scale random and reposition
+			var scale = 1 + Math.random()*2*(i+1)*0.05;
+			// console.log("scale: " + scale);
+			primes[i][j].scaling.y = scale;
+			primes[i][j].position = new BABYLON.Vector3(x, scale/2, z);
+
+			// console.log(primes[i][j]);
 		}
 	}
-
-
-	// return the created scene
 	return scene;
 }
 
--- prismas/utils.js
+++ prismas/utils.js
@@ -1,13 +1,11 @@
-var create_cube = function(scene, size, x, y, z)
+var create_cube = function(scene, size)
 {
 	var cube = BABYLON.Mesh.CreateBox("box", size, scene);
-	cube.position = new BABYLON.Vector3(x, y, z);
 	return cube;
 }
 
-var create_cylinder = function(height, diameter, num_border, scene, updatable)
+var create_cylinder = function(scene, height, diameter, num_border)
 {
-	//Creation of a cylinder
 	//(name, height, diameter, tessellation, scene, updatable)
 	var cylinder = BABYLON.Mesh.CreateCylinder("cylinder", height, diameter, diameter, num_border, 1, scene);
 	return cylinder;
@@ -36,8 +34,9 @@ var create_ribon = function(scene)
 
 var camera1 = function(scene, canvas)
 {
-	var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-10), scene);
-	camera.setTarget(BABYLON.Vector3.Zero());
+	var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 0, 0), scene);
+	// camera.setTarget(new BABYLON.Vector3(2.5, 0, 2.5));
+	camera.setTarget(new BABYLON.Vector3(0, 0, 0));
 	camera.attachControl(canvas, false);
 	return camera;
 }
@@ -45,6 +44,7 @@ var camera1 = function(scene, canvas)
 var camera2 = function(scene, canvas)
 {
 	var camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 8, 50, BABYLON.Vector3.Zero(), scene);
+	camera.setPosition(new BABYLON.Vector3(0, 0, -30));
 	camera.attachControl(canvas, true);
 	return camera;
 }
@@ -53,4 +53,24 @@ var randomFloat = function(min, max) {
     var highlightedNumber = Math.random() * (max - min) + min;
     return highlightedNumber;
 }
+
+/** * Zoom / dezoom in 2D view * @param {int} direction The mouse scroll (120 = one tick on scroll) */
+// var MouseWheelHandler = function(direction) 
+// {    
+// 	var zoom = direction / 120;
+// 	topCamera.orthoLeft += zoom *1.4;
+// 	topCamera.orthoRight -= zoom *1.4;
+// 	topCamera.orthoBottom += zoom *0.9;
+// 	topCamera.orthoTop -= zoom *0.9;
+// }
+
+
+
+
+
+
+
+
+
+
 
